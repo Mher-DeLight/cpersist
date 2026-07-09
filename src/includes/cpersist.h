43ca396c3564471d25dca5211ff4ebfe5018dd0c
Mher DeLight <mahirzamil2012@gmail.com>
2026-07-09 12:05:15 +0300

    added a SaveManager to the cpersist header

--- src/includes/cpersist.h
+++ src/includes/cpersist.h
@@ -1,3 +1,14 @@
 #pragma once
+#include <string>
 
+// debugging
 void say_hello();
+
+class SaveManager {
+private:
+    std::string current_file;
+public:
+    void change_file();     // changes the current file
+    void new_file();        // creates a new file
+    void use_new_file();    // creates a new file and switches to it
+};
