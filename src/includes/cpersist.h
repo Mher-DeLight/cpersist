3684563b184bf370a8e4223b80f0f8d9437e9089
Mher DeLight <mahirzamil2012@gmail.com>
2026-07-09 12:20:06 +0300

    added parameters to file functions

--- src/includes/cpersist.h
+++ src/includes/cpersist.h
@@ -8,7 +8,7 @@ class SaveManager {
 private:
     std::string current_file;
 public:
-    void change_file();     // changes the current file
-    void new_file();        // creates a new file
-    void use_new_file();    // creates a new file and switches to it
+    void change_file(const std::string& new_file);     // changes the current file
+    void create_new_file(const std::string& new_file);        // creates a new file
+    void use_new_file(const std::string& new_file);    // creates a new file and switches to it
 };
