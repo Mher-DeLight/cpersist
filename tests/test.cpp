e05006f5508afbfca3c2e6c1fa19df5aa305cfa0
Mher DeLight <mahirzamil2012@gmail.com>
2026-07-20 12:22:01 +0300

    modified cmakelists

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,13 +4,4 @@
 int main () {
     saveMgr.enable_encryption(true);
     saveMgr.set_encryption_key("wowyoureallyfoundit");
-
-    std::vector<int> myvec = {3, 5, 2 , 1};
-
-    saveMgr.open("myfile");
-    saveMgr.write("vec", myvec);
-    saveMgr.commit();
-    for (auto el : saveMgr.read<std::vector<int>>("vec")) {
-        std::cout << el << std::endl;
-    }
 }
